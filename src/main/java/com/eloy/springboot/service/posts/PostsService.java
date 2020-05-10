30f72b6dc19ab500e0b4046a10cde62c9962a454
박인호 <eloy@jober.io>
2020-05-10 21:40:53 +0900

    implements update post api
    domain (repository) 생성
    service 생성
    controller (api) 생성
    DTO 생성
    
    [기타 작업]
    test code 작성

--- src/main/java/com/eloy/springboot/service/posts/PostsService.java
+++ src/main/java/com/eloy/springboot/service/posts/PostsService.java
@@ -4,6 +4,7 @@ import com.eloy.springboot.domain.posts.Posts;
 import com.eloy.springboot.domain.posts.PostsRepository;
 import com.eloy.springboot.web.dto.PostsResponseDto;
 import com.eloy.springboot.web.dto.PostsSaveRequestDto;
+import com.eloy.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,16 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
     }
 
     public PostsResponseDto findById(Long id) {
--- src/main/java/com/eloy/springboot/web/PostsApiController.java
+++ src/main/java/com/eloy/springboot/web/PostsApiController.java
@@ -3,6 +3,7 @@ package com.eloy.springboot.web;
 import com.eloy.springboot.service.posts.PostsService;
 import com.eloy.springboot.web.dto.PostsResponseDto;
 import com.eloy.springboot.web.dto.PostsSaveRequestDto;
+import com.eloy.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +22,11 @@ public class PostsApiController {
     public PostsResponseDto findById (@PathVariable Long id) {
         System.out.println(postsService.findById(id));
         return postsService.findById(id);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
     }
 
 }
