a1b0b01fc8af795c8675c927b338ca1ae6e12cc4
박인호 <eloy@jober.io>
2020-05-10 17:28:24 +0900

    implements create post api
    domain (repository) 생성
    service 생성
    controller (api) 생성
    DTO 생성
    
    [기타 작업]
    test code 작성

---
+++ src/main/java/com/eloy/springboot/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package com.eloy.springboot.service.posts;
+
+import com.eloy.springboot.domain.posts.PostsRepository;
+import com.eloy.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
