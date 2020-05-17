ffc6bdceaa0a8076d6af8e63817f54ad0afc35fa
박인호 <eloy@jober.io>
2020-05-17 16:39:42 +0900

    create LoginUser annotation
    getMapping 을 하는 메소드에서 세션 값이 필요할 때 마다
    불러오는 것은 중복이 발생하는 좋지 않은 코드
    (SessionUser user = (SessionUser) httpSession.getAttribute("user"))
    매번 같은 코드를 생성하지 않기 위해 커스텀 어노테이션 생성
    
    1. 실제 어노테이션 객체(인터페이스) 생성
    LoginUser.java
    2. 조건에 맞는 경우 메소드가 있다면 구현체가 지정한 값으로  메소드의 파라메터로 전달
    LoginUserArgumentResolver.java
    3. LoginUserArgumentResolver 를 스프링에서 인식할 수 있도록 설정
    WebConfig.java

--- src/main/java/com/eloy/springboot/web/IndexController.java
+++ src/main/java/com/eloy/springboot/web/IndexController.java
@@ -3,6 +3,7 @@ package com.eloy.springboot.web;
 import javax.mail.Session;
 import javax.servlet.http.HttpSession;
 
+import com.eloy.springboot.config.auth.LoginUser;
 import com.eloy.springboot.config.auth.dto.SessionUser;
 import com.eloy.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
 //        model.addAttribute("post", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
